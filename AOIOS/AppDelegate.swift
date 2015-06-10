4f8c16d2fe7b2484540cfb49df3aa925f4777f65
刘洪彬 <artwebs@126.com>
2015-06-10 15:42:32 +0800

    完成测试Category 测试工作

--- AOIOS/AppDelegate.swift
+++ AOIOS/AppDelegate.swift
@@ -16,7 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-//        self.log("test")
+        self.log("test")
+//        var obj=DemoTest()
+//        obj.log("-----")
+        print(Utils.base64EncodeWithString("11111"))
         let splitViewController = self.window!.rootViewController as! UISplitViewController
         let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController
         navigationController.topViewController.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem()
